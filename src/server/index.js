321ba2b2a37f91ad48287d72013a7371d84acfda
shivam88877777 <shivam88877777@gmail.com>
2020-04-18 00:10:37 +0530

    second commit

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,16 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+var aylien = require("aylien_textapi");
+// set aylien API credentias
+var textapi = new aylien({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+});
+
+
+
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
